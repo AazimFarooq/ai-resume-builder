fd152b055b5b1a105365b1db376aaa16294d6820
Aazim <45119212+AazimFarooq@users.noreply.github.com>
2025-03-16 14:22:05 +0530

    Update simple.test.tsx

--- __tests__/simple.test.tsx
+++ __tests__/simple.test.tsx
@@ -1,7 +1,8 @@
+import "@testing-library/jest-dom"
+
 describe("Simple test", () => {
-    it("passes a basic test", () => {
-      expect(1 + 1).toBe(2)
-    })
+  it("passes a basic test", () => {
+    expect(1 + 1).toBe(2)
   })
-  
-  
+})
+
