eb23ef20e457b4be7c39a3acd86d410ff3b03607
Aazim <45119212+AazimFarooq@users.noreply.github.com>
2025-05-25 13:56:04 +0530

    Delete app/components.tsx

--- app/components.tsx
+++
@@ -1,7 +0,0 @@
-export { default as Features } from "./components/features"
-export { default as Hero } from "./components/hero"
-export { default as HowItWorks } from "./components/how-it-works"
-export { default as Pricing } from "./components/pricing"
-export { default as Testimonials } from "./components/testimonials"
-export { default as Footer } from "./components/footer"
-
